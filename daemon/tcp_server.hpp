a1e56baa7b61a4d04946c6829c386129c9f62823
dodicidodici <dodicilosanno@gmail.com>
2024-09-06 15:08:14 +0200

    re-order members of `tcp_server` + change `m_connections` to `std::unordered_map`

--- daemon/tcp_server.hpp
+++ daemon/tcp_server.hpp
@@ -8,12 +8,6 @@
 namespace deflux::net {
 
 class tcp_server {
-    asio::io_context m_executor;
-    std::vector<std::shared_ptr<tcp_connection>> m_connections{};
-    asio::ip::tcp::acceptor m_acceptor;
-
-    asio::awaitable<void> handle_new_connections();
-
 public:
     explicit tcp_server(const asio::ip::tcp::endpoint& endpoint) : m_acceptor(m_executor, endpoint) {  }
     virtual ~tcp_server() = default;
@@ -23,6 +17,15 @@ public:
 
     virtual void on_message_received(std::vector<uint8_t> raw_message, tcp_connection::id_t id) = 0;
     virtual void on_connection_close(tcp_connection::id_t id) = 0;
+
+protected:
+    std::unordered_map<tcp_connection::id_t, std::shared_ptr<tcp_connection>> m_connections{};
+
+private:
+    asio::io_context m_executor;
+    asio::ip::tcp::acceptor m_acceptor;
+
+    asio::awaitable<void> handle_new_connections();
 };
 
 }
