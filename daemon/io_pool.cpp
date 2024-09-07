7b069a7483341464fb03689bfebd2b938e08448c
dodicidodici <dodicilosanno@gmail.com>
2024-09-07 15:03:14 +0200

    implement `io_pool`
    multiple strands for better concurrency

---
+++ daemon/io_pool.cpp
@@ -0,0 +1,54 @@
+#include "io_pool.hpp"
+
+namespace deflux {
+
+void run_executor(asio::io_context &ctx) {
+    const asio::executor_work_guard idle_work = make_work_guard(ctx);
+    ctx.run();
+}
+
+void io_pool::run() const {
+    assert(!m_executors.empty());
+    std::vector<std::thread> execution_threads{};
+
+    for (auto i = 0; i < m_pool_size - 1; i++) {
+        asio::io_context& ctx = *m_executors.at(i);
+        execution_threads.emplace_back([&, this]{ run_executor(ctx); });
+    }
+
+    run_executor(*m_executors.back());
+
+    for (auto& thread : execution_threads) {
+        if (thread.joinable())
+            thread.join();
+    }
+}
+
+void io_pool::stop() const {
+    for (const auto& ctx : m_executors) {
+        ctx->stop();
+        delete ctx;
+    }
+}
+
+asio::io_context& io_pool::get_executor() {
+    auto& ctx = *m_executors.at(m_current_executor);
+    m_current_executor = (m_current_executor + 1) % m_pool_size;
+
+    return ctx;
+}
+
+size_t io_pool::size() const {
+    return m_pool_size;
+}
+
+void io_pool::instantiate_executors() {
+    assert(m_pool_size);
+
+    for (auto i = 0; i < m_pool_size; i++) {
+        auto* ctx = new asio::io_context();
+        m_executors.push_back(ctx);
+    }
+}
+
+}
---
+++ daemon/io_pool.hpp
@@ -0,0 +1,73 @@
+#ifndef IO_POOL_HPP
+#define IO_POOL_HPP
+
+#include <stdexcept>
+#include <thread>
+#include <vector>
+#include <asio/io_context.hpp>
+
+namespace deflux {
+
+/**
+ * Pool of multiple asio strands.
+ *
+ * By default, the number of strands is equal to the amount of hardware threads.
+ */
+class io_pool {
+public:
+    io_pool(const io_pool& other) = delete;
+
+    /**
+     * Creates an `io_pool` with a default size of std::thread::hardware_concurrency, or 1,
+     * if hardware concurrency cannot be determined.
+     */
+    io_pool() {
+        instantiate_executors();
+    }
+
+    /**
+     * Creates an `io_pool` with `size` executors
+     *
+     * @param size non-zero size of the pool
+     * @throws std::invalid_argument if size is 0
+     */
+    explicit io_pool(const size_t& size) : m_pool_size(size) {
+        if (size == 0)
+            throw std::invalid_argument("io_pool size cannot be 0");
+
+        instantiate_executors();
+    }
+
+    /**
+     * Starts the `io_pool` event loop.
+     *
+     * @note Spawns `size()` - 1 threads, and uses the caller thread as executors.
+     */
+    void run() const;
+
+    /**
+     * Stops the event loop.
+     */
+    void stop() const;
+
+    /**
+     * Gets the next executor to be used. Selected using round robin.
+     *
+     * @return next executor
+     */
+    [[nodiscard]] asio::io_context& get_executor();
+    [[nodiscard]] size_t size() const;
+
+private:
+    // FIXME consider using less threads as a default, say, 75% of available threads
+    size_t m_pool_size = std::max<size_t>(1, std::thread::hardware_concurrency());
+    size_t m_current_executor = 0;
+    // store as a pointer because the copy constructor of asio::io_context is deleted
+    std::vector<asio::io_context*> m_executors{};
+
+    void instantiate_executors();
+};
+
+}
+
+#endif //IO_POOL_HPP
