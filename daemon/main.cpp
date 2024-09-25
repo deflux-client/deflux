0d87f5d7da3d199417f6a99775dea55e2925c9be
dodicidodici <dodicilosanno@gmail.com>
2024-09-25 18:34:13 +0200

    start implementing request handling

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -2,12 +2,13 @@
 
 #include <asio.hpp>
 #include <deflux/protocol.hpp>
-#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 #include "tcp_server.hpp"
 
-class daemon : public deflux::net::tcp_server {
+namespace deflux {
+
+class daemon : public net::tcp_server {
 public:
     daemon()
         : tcp_server(asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 2347))
@@ -17,23 +18,60 @@ public:
 protected:
     void on_message_received(std::vector<uint8_t> raw_message, std::shared_ptr<net::tcp_connection> connection) override
     {
-        const std::string message{ raw_message.begin(), raw_message.end() };
-        spdlog::debug("connection {}: {}", connection->id(), message);
+        auto f = [this, r = move(raw_message), connection = move(connection)]() mutable {
+            auto res = nlohmann::json::to_msgpack(handle_client_request(r));
+            connection->send(std::move(res));
+        };
 
-        connection->send(raw_message);
+        // defer execution to another thread and free up the caller strand
+        post(m_thread_pool, f);
     }
 
-    void on_connection_close(deflux::net::tcp_connection::id_t id) override
+    void on_connection_close(net::tcp_connection::id_t id) override
     {
         spdlog::info("connection {} closed", id);
     }
+
+private:
+    asio::thread_pool m_thread_pool = { std::thread::hardware_concurrency() };
+
+    void shutdown()
+    {
+        stop();
+    }
+
+    nlohmann::json handle_client_request(const std::vector<uint8_t>& raw)
+    {
+        unparsed_message_t request{};
+
+        try {
+            request = nlohmann::json::from_msgpack(raw);
+        } catch (const std::exception& e) {
+            spdlog::error("error while parsing request: {}", e.what());
+            return make_response(error_reason::invalid_request);
+        }
+
+        switch (request.type) {
+        case message_type::shutdown:
+            shutdown();
+            [[fallthrough]];
+        case message_type::ping:
+            return make_response(empty_t{}, request.tag);
+        default:
+            return make_response(error_reason::no_such_method, request.tag);
+        }
+
+        assert(false);
+    }
 };
+
+}
 
 int main()
 {
-    daemon d{};
+    deflux::daemon daemon{};
 
-    d.run();
+    daemon.run();
     spdlog::shutdown();
 
     return EXIT_SUCCESS;
